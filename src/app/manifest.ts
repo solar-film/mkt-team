33338c477258302cdb2cbc19a6807225616f98d6
Solar Film MKT <marketing@solar-film.com>
2026-06-13 19:42:36 +0700

    Add custom mobile app icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#6366f1',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/icon.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   }
