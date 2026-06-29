0da171f55d9c8d7db0f558abdc1561d56dbd96fc
OILLY <supaporn.gfs@gmail.com>
2026-06-29 12:42:38 +0700

    fix: remove invalid eslint config option from next.config.ts which is fatal on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: any = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
